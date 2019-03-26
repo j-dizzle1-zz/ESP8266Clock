eca22bc32c7294a65ecb113517974dda955a6f02
j-dizzle1 <eastjayhawk@gmail.com>
2019-03-26 18:54:28 -0400

    Added more to the code, structure starting to begin with the esp code

---
+++ ESP8266_code/ESP8266_code.ino
@@ -0,0 +1,35 @@
+#include <FastLED.h>
+const int daylightButton = 2; //This will be to as buttonStateOne for logic
+const int nighttimeButton = 3; //This will be buttonStateTwo
+const int onButton = 4; //buttonStateThree
+const int offButton = 5; //buttonStateFour
+int buttonStateOne = 0;
+int buttonStateTwo = 0;
+int buttonStateThree = 0;
+int buttonStateFour = 0;
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
+
+void daylight(){
+  
+}
+
+void nightTime(){
+  
+}
+
+void off(){
+   
+}
+
+void on(){
+  
+}
